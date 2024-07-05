c7e659d527048f17356332d5a2035ada45aaed17
maicogeuihghguerh <127985262+maicogeuihghguerh@users.noreply.github.com>
2024-07-05 11:01:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+
+const perguntas = [
+    {
+        enunciado: "verdadeiro que o ciclo da soja e de 120 dias",
+        alternativas: ["verdadeiro ", "falso "],
+    },
+    {
+        enunciado: "qual tipo de adubo e usado no plantio da soja",
+        alternativas: ["npk", "ureia"],
+    },
+    {
+        enunciado: "qual a faxa de producao de soja no parana",
+        alternativas: [" 18,23 milhões de toneladas", "40,38 milhões de toneladas de grãos"],
+    },
+];
+
+let atual = 0;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual < perguntas.length) {
+        const perguntaAtual = perguntas[atual];
+        caixaPerguntas.textContent = perguntaAtual.enunciado;
+        mostraAlternativas(perguntaAtual.alternativas);
+    } else {
+        caixaPerguntas.textContent = '';
+        caixaAlternativas.innerHTML = '';
+        textoResultado.textContent = `Você respondeu todas as perguntas! Sua história: ${historiaFinal}`;
+    }
+}
+
+function mostraAlternativas(alternativas) {
+    caixaAlternativas.innerHTML = ''; // Limpa as alternativas anteriores
+    alternativas.forEach(alternativa => {
+        const botaoAlternativa = document.createElement("button");
+        botaoAlternativa.textContent = alternativa;
+        botaoAlternativa.addEventListener('click', () => {
+            respostaSelecionada(alternativa);
+        });
+        caixaAlternativas.appendChild(botaoAlternativa);
+    });
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    historiaFinal += opcaoSelecionada + ' ';
+    atual++;
+    mostraPergunta();
+}
+
+mostraPergunta();
